bc758bf4df956d2411c632dbb2e601d98389cd29
Sumeet Singh <ortuna@gmail.com>
2014-07-01 00:20:38 -0400

    Updating mailer

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -8,10 +8,28 @@ class NotificationsMailer < ActionMailer::Base
 
   def invite(user_invitation)
     invite = find_invite(user_invitation)
+    user   = invite.user
+
+    @first_name          = user.first_name
+    @assessment_name     = invite.assessment.name 
+    @assessment_district = invite.assessment.district.name
+    @owner_name          = invite.assessment.user.first_name
+    @message             = invite.assessment.message
+    @due_date            = invite.assessment.due_date
+    @avatar              = user.avatar || default_avatar
+    @invite_link         = invite_url(invite.token)
     mail(to: invite.email)
   end
 
   private
+  def invite_url(token)
+    "#{ActionController::Base.relative_url_root}/#/invitation/#{token}"
+  end
+
+  def default_avatar
+    ActionController::Base.helpers.asset_path('fallback/default.png')
+  end
+
   def find_invite(invite_id)
     UserInvitation.find(invite_id)
   end

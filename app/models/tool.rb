9eefeb32957c4ab7c94199ab09848559bc7f0ff8
Sean Perkins <sean@mobility-labs.com>
2014-07-15 13:35:46 -0500

    Add models for toolkit

---
+++ app/models/tool.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: tools
+#
+#  id                  :integer          not null, primary key
+#  title               :string(255)
+#  description         :text
+#  url                 :string(255)
+#  default             :boolean
+#  display_order       :integer
+#  tool_category_id    :integer
+#  tool_subcategory_id :integer
+#  user_id             :integer
+#
+
+class Tool < ActiveRecord::Base
+  belongs_to :tool_category
+  belongs_to :tool_subcategory
+  belongs_to :user
+end
---
+++ app/models/tool_category.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: tool_categories
+#
+#  id            :integer          not null, primary key
+#  title         :string(255)
+#  display_order :integer
+#  tool_phase_id :integer
+#
+
+class ToolCategory < ActiveRecord::Base
+  belongs_to :tool_phase
+end

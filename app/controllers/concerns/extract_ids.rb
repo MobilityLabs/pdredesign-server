9c844322b5e3395df208c7f53f07f20698e38933
Sumeet Singh <ortuna@gmail.com>
2014-08-18 13:05:42 -0400

    Fixnum vs a list of ids

--- app/controllers/concerns/extract_ids.rb
+++ app/controllers/concerns/extract_ids.rb
@@ -3,6 +3,12 @@ module ExtractIds
 
   def extract_ids_from_params(key)
     return '' unless params[key]
+    return fixnum_to_ids(params[key]) if params[key].is_a?(Numeric)
     params[key].split(",")
+  end
+
+  private
+  def fixnum_to_ids(num)
+    [num] 
   end
 end

bce7fe00d40f61664020c429e77b45e4d97d8eb8
Sumeet Singh <ortuna@gmail.com>
2014-09-17 11:22:47 -0400

    added SlideUploader for ImageSlides

---
+++ app/uploaders/slide_uploader.rb
@@ -0,0 +1,13 @@
+class SlideUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :fog
+
+  def store_dir
+    "#{model.class.to_s.underscore}/#{model.id}/#{mounted_as}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end

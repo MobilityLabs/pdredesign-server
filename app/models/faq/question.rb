295ea4eb419904c3dd851244bbed44da2727cfe9
Sumeet Singh <ortuna@gmail.com>
2014-10-09 10:28:17 -0400

    Added validators

--- app/models/faq/question.rb
+++ app/models/faq/question.rb
@@ -1,2 +1,5 @@
 class Faq::Question < ActiveRecord::Base
+  validates :content, presence: true
+  validates :answer,  presence: true
+  validates :tool_id, presence: true
 end
--- spec/models/faq/question_spec.rb
+++ spec/models/faq/question_spec.rb
@@ -1,9 +1,14 @@
-require 'rails_helper'
+require 'spec_helper'
 
 describe Faq::Question do
   let(:subject) { Faq::Question }
-  it 'requires :content' do
-    
+  it 'requires :content, :answer, :tool_id' do
+    question = subject.new()
+    question.valid?
+
+    expect(question.errors[:content]).not_to be_empty
+    expect(question.errors[:answer]).not_to  be_empty
+    expect(question.errors[:tool_id]).not_to be_empty
   end
 end
 

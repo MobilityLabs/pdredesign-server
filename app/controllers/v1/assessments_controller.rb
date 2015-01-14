1d3c2c08931fb040c1dc2160a5d5d7fcbede1264
lmanotas <luis@mobility-labs.com>
2015-01-14 13:49:58 -0500

    Cache_path as a path

--- app/controllers/v1/assessments_controller.rb
+++ app/controllers/v1/assessments_controller.rb
@@ -1,7 +1,7 @@
 class V1::AssessmentsController < ApplicationController
   before_action :authenticate_user!
 
-  caches_action :index, :show, cache_path: Proc.new { |c| c.current_user.id }
+  caches_action :index, :show, cache_path: Proc.new { |c| "/assessments/#{c.current_user.id}" }
 
   def index
     @assessments = user_assessments
@@ -44,8 +44,8 @@ class V1::AssessmentsController < ApplicationController
     assign_current_user_as_participant(@assessment) if current_user.district_member?
 
     if @assessment.save
-      expire_action action: :show, cache_path: Proc.new { |c| c.current_user.id }
-      expire_action action: :index, cache_path: Proc.new { |c| c.current_user.id }
+      expire_action action: :show, cache_path: Proc.new { |c| "/assessments/#{c.current_user.id}" }
+      expire_action action: :index, cache_path: Proc.new { |c| "/assessments/#{c.current_user.id}" }
       render :show
     else
       @errors = @assessment.errors

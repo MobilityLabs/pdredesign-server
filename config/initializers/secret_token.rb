423194d7f66094c4dd0f4a458f090d8450da0445
lmanotas <luis@mobility-labs.com>
2015-03-10 19:41:29 -0500

    Getting secret_token in the secrets rails configuration

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PdrServer::Application.config.secret_key_base = 'a9c074f6ba99338c7c50a0664958a5f5fffeaf0370583aacd489da8a9f6eff3ac61ddd7bf098d070ecc832efbff18e3571536f3c3ee4c6938d14cf4308f78c8d'
+PdrServer::Application.config.secret_key_base = Rails.application.secrets.secret_key_base
